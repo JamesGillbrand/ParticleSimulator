348a599fc74664a5dd114bf234868d2fb257d3a7
jgillbrand <jgillbrand@uchicago.edu>
2025-02-07 18:35:11 -0600

    Splitting up file

---
+++ functions.py
@@ -0,0 +1,28 @@
+from points import Point, Swarm
+
+
+def gravity(particle: Point, points: list[Point]) -> Point:
+    fx, fy = (0, 0)  # Initial forces
+    G: float = 6.67 * (10 ** -11)  # Gravitational Constant
+    t: float = 1000  # Time between calculations/jumps
+
+    # Iterate through points adding up gravitational force
+    for point in points:
+        if point != particle:
+            r = particle.distance(point)
+            dir_x, dir_y = particle.direction(point)
+
+            fx += ((G * particle.mass * point.mass) / (r ** 2)) * dir_x
+            fy += (G / (r ** 2)) * dir_y
+
+    vx = fx * t + particle.vx
+    vy = fy * t + particle.vy
+
+    fx = fx + particle.fx
+    fy = fy + particle.fy
+
+    x = vx * t + 0.5 * (fx * (t ** 2)) + particle.x
+    y = vy * t + 0.5 * (fy * (t ** 2)) + particle.y
+
+    new_point = Point((x, y), (vx, vy), (fx, fy))
+    return new_point
